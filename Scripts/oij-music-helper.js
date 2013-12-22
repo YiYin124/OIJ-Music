ae11a263c5c7d39b54ba58d9cab8a88372d73f51
YiYin <yiyin124@gmail.com>
2013-12-22 08:16:24 +0800

    增加了一些提示

---
+++ Scripts/oij-music-helper.js
@@ -0,0 +1,12 @@
+﻿// By 既是安安
+// http://www.jsann.com/post/JS_GET_parameters_to_obtain.html
+function getArgs() {
+    var args = {};
+    var match = null;
+    var search = decodeURIComponent(location.search.substring(1));
+    var reg = /(?:([^&]+)=([^&]+))/g;
+    while ((match = reg.exec(search)) !== null) {
+        args[match[1]] = match[2];
+    }
+    return args;
+}
